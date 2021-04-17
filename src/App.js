cf42a11df12f4c42b744af2036b0f2163ba1ad5d
Rigo Gonzalez <rigognzlz@gmail.com>
2021-04-18 02:41:56 +0800

    app structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
-import './App.css';
+import './styles/App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import {Navbar, Badge, Container, Row, Col} from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+		<header>
+			<Navbar bg="dark" variant="dark">
+				<Navbar.Brand>eStore</Navbar.Brand>
+				<Navbar.Toggle />
+				<Navbar.Collapse className="justify-content-end">
+					<Navbar.Text>
+						<FontAwesomeIcon icon={faShoppingCart} /> <Badge variant="danger">9</Badge>
+					</Navbar.Text>
+				</Navbar.Collapse>
+			</Navbar>
+		</header>
+
+		<div className="mb-2 mt-2">
+			<Container>
+				<Row>
+					<Col>
+						Product list here
+					</Col>
+				</Row>
+			</Container>
+
+		</div>
+
+		<footer>
+			<p className="text-center">&copy; 2021 jrcgonzalez.com</p>
+		</footer>
+
     </div>
   );
 }
